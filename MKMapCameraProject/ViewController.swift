3dafe89632462efffec6517b9180ea08419d1b2d
akinolussanya <66926466+akinolussanya@users.noreply.github.com>
2021-09-06 19:08:28 +0100

    A market feature that shows a satellite imagery of a favourite place of mine. I have  made use of the MKmapCamera feature here and initialised it with various attributes like distance etc

--- MKMapCameraProject/ViewController.swift
+++ MKMapCameraProject/ViewController.swift
@@ -6,13 +6,35 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    // initialise mapview here
+    let map = MKMapView()
+    
+    let southBankLocation = CLLocationCoordinate2D(latitude: 51.5021033, longitude: -0.1224091)
+    
+    var camera = MKMapCamera()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        map.delegate = self
+        
+        camera = MKMapCamera(lookingAtCenter: southBankLocation, fromDistance: 3000, pitch: 400, heading: 45.0)
+        map.camera = camera
+        map.mapType = .satelliteFlyover
+        map.frame = view.bounds
+        view.addSubview(map)
+       
     }
+    
+    
+ 
+    
+    
 
 
 }
